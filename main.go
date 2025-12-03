1f85cd384837413285f81ed25972e51cf40058b3
Othayz <thairome.413b@gmail.com>
2025-12-03 11:36:19 -0300

    Adds a Pizza structure with the fields id, name, and price.

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+type Pizza struct {
+	id	int
+	nome string
+	preco float64
+}
+
 func main() {
 	
 	var nomePizzaria string = "Pizzaria GO"
-	
+	var toscana Pizza
 	instagram, telefone := "@pizzaria_GO", 123456789
 	
 	fmt.Printf("Nome da pizzaria: %s (instagram: %s) - Telefone: %d", nomePizzaria, instagram, telefone)
