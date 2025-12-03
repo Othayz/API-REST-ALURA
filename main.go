d6bc7eef64f074b28e4726b5b06a348e0778eba0
Othayz <thairome.413b@gmail.com>
2025-12-03 12:05:27 -0300

    Add dependencies and configure the server to display the pizza list.

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+
+)
 
 type Pizza struct {
 	id	int
@@ -10,10 +14,18 @@ type Pizza struct {
 
 func main() {
 	
+	router := gin.Default()
+	router.GET("/pizzas", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"pizzas": "Toscana", "Marquerita", "Atum com queijo",
+		})
+	})
+
 	var pizzas = []Pizza{
 		{id: 1, nome: "Toscana", preco: 25.00},
 		{id: 2, nome: "Marquerita", preco: 30.00},
 		{id: 3, nome: "Atum com queijo", preco: 35.00},
 	}
-	fmt.Println(pizzas)	
+	fmt.Println(pizzas)
+	router.Run()
 }
